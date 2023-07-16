ed012c4e5ed795aee4a977c819d49d7b5c4fb7c3
Savaş Tanrıverdi <savas.tanriverdi@outlook.com>
2023-07-16 22:39:41 +0300

    Files

---
+++ app/src/main/java/com/savastanriverdi/artbookjava/Art.java
@@ -0,0 +1,12 @@
+package com.savastanriverdi.artbookjava;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
